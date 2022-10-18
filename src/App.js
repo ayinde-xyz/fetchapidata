40ca9fd3807febed9dcddc87611fd7cd17739b78
Abdulrahman Ayinde <ayindeabdulrahman16@gmail.com>
2022-10-18 12:09:21 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import Form from './Form'
+import Lists from './Lists'
+import { useState, useEffect } from 'react'
+import Table from './Table'
+function App() {
+  const API_URL = 'https://jsonplaceholder.typicode.com'
+  const [reqType, setReqType] = useState('users')
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(`${API_URL}/${reqType}`)
+        const data = await response.json()
+        console.log(data)
+        setItems(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchItems()
+  },[reqType])
+ 
+  return (
+    <div className="App">
+      <Form reqType={reqType} setReqType={setReqType}/>
+      {/* <Lists items={items} /> */}
+      <Table items={items}/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Lists.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ListItem from './ListItem'
+
+const Lists = ({items}) => {
+  return (
+    <ul>
+        {items.map(item => (
+            <ListItem key={item.id} item={item}/>
+        ))}
+    </ul>
+  )
+}
+
+export default Lists
